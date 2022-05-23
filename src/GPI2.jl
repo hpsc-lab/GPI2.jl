6c9ca767d7d2ca23bec21f23a2bfc7b3195aa62d
Michael Schlottke-Lakemper <michael@sloede.com>
2022-05-23 08:46:13 +0200

    Add experimental warning to docstrings

--- src/GPI2.jl
+++ src/GPI2.jl
@@ -76,6 +76,9 @@ end
     gaspi_logger()
 
 Run the `gaspi_logger` tool of the GPI-2 library.
+
+!!! warning "Experimental implementation"
+    This is an experimental feature and may change in future releases.
 """
 function gaspi_logger()
   try
@@ -92,6 +95,9 @@ end
     gaspi_run()
 
 Run the `gaspi_run` tool of the GPI-2 library.
+
+!!! warning "Experimental implementation"
+    This is an experimental feature and may change in future releases.
 """
 function gaspi_run()
   try
