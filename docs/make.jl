bdcec6183457d8f2e6a9c819726ac16a19c965de
Michael Schlottke-Lakemper <michael@sloede.com>
2022-05-23 10:31:25 +0200

    Add documentation using Documenter.jl

---
+++ docs/make.jl
@@ -0,0 +1,36 @@
+using Documenter
+import Pkg
+using GPI2
+
+# Define module-wide setups such that the respective modules are available in doctests
+DocMeta.setdocmeta!(GPI2,     :DocTestSetup, :(using GPI2);     recursive=true)
+
+# Make documentation
+makedocs(
+    # Specify modules for which docstrings should be shown
+    modules = [GPI2],
+    # Set sitename to GPI2
+    sitename="GPI2.jl",
+    # Provide additional formatting options
+    format = Documenter.HTML(
+        # Disable pretty URLs during manual testing
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        # Explicitly add favicon as asset
+        # assets = ["assets/favicon.ico"],
+        # Set canonical URL to GitHub pages URL
+        canonical = "https://hlrs-tasc.github.io/GPI2.jl/dev"
+    ),
+    # Explicitly specify documentation structure
+    pages = [
+        "Home" => "index.md",
+        "Reference" => "reference.md",
+        "License" => "license.md"
+    ],
+    strict = true # to make the GitHub action fail when doctests fail, see https://github.com/neuropsychology/Psycho.jl/issues/34
+)
+
+deploydocs(
+    repo = "github.com/hlrs-tasc/GPI2.jl",
+    devbranch = "main",
+    push_preview = true
+)
---
+++ docs/src/reference.md
@@ -0,0 +1,9 @@
+# GPI2.jl API
+
+```@meta
+CurrentModule = GPI2
+```
+
+```@autodocs
+Modules = [GPI2, GPI2.LibGPI2]
+```
